5af7577a0fe9cc293d8c75a6ad65baf7a7dd4245
Juris Raudulis <juris.raudulis@outlook.com>
2023-10-19 18:52:39 +0100

    Reuploading files

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+let css = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("gradient");
+let rndmBtn = document.getElementById("random");
+
+
+const setRndmBckgr = ()=>{
+		const rndmCol = Math.floor(Math.random()*16777215).toString(16);
+		const rndmCol2 = Math.floor(Math.random()*16777215).toString(16);
+
+		color1.value = "#" + rndmCol;
+		color2.value= "#" + rndmCol2;
+
+		body.style.background = 
+		"linear-gradient(to right, " + 
+		color1.value 
+		+ ", " 
+		+ color2.value 
+		+ ")";
+
+		css.textContent = body.style.background + ";";
+}
 
 function setGradient() {
 	body.style.background = 
@@ -14,6 +33,11 @@ function setGradient() {
 	css.textContent = body.style.background + ";";
 }
 
+setRndmBckgr();
+
+rndmBtn.addEventListener("click", setRndmBckgr);
+
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
